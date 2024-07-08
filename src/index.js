2f55caed1c3be4fc6b725c8c82c410c87aa3dd9b
aggreyrc <aggreyronoh@gmail.com>
2024-07-09 00:18:00 +0300

    task-lister-mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let newTaskDescription = document.getElementById("new-task-description").value;
+    handleCreateTask(newTaskDescription);
+    form.reset();
+  })
 });
+
+function handleCreateTask(task){
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", handleDelete);
+  btn.textContent = "x"
+  li.textContent = `${task} `;
+  li.appendChild(btn);
+  document.getElementById("tasks").appendChild(li);
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
